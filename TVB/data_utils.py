b94f0dbe84b8944af9471ea9e3ba33eb8af0bcbf
Sathvik <bloodraven66@github.com>
2022-06-13 17:38:14 +0530

    build custom dataset class

---
+++ TVB/data_utils.py
@@ -0,0 +1,12 @@
+import torch
+from torch import nn
+
+class TorchDataset(nn.Dataset):
+    def __init__(self, *args, **kwargs):
+        pass
+    
+    def __len__(self):
+        return
+    
+    def __call__(self):
+        return
